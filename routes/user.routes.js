82fb4e4264b08c34bf55e6c466abaf89e8b747d0
misha441 <mihadebelyak@gmail.com>
2023-03-12 18:50:17 +0200

    package2

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const {Router} = require("express")
 const router = Router()
 const sqlite3 = require('sqlite3').verbose()
 const fs = require("fs")
-const mongoose = require("mongoose")
+//const mongoose = require("mongoose")
 // const Post = require(".././models/post")
 //
 // const db = "mongodb+srv://mihadebelyak:20OimFG9cKO7NI7C@cluster0.ycip03x.mongodb.net/statsapp?retryWrites=true&w=majority"
